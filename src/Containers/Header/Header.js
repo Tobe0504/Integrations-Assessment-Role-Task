d3f0751728e00e708b006f89277c387f079a6fa5
Tobe0504 <ezimorahtobenna@gmail.com>
2023-03-09 18:52:24 +0100

    work on responsiveness

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -1,14 +1,68 @@
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./Header.module.css";
 
 const Header = () => {
+  // Utilities
+
+  const openSideMenu = () => {
+    document.getElementById("sideMenu").style.width = "100%";
+  };
+
+  const closeSideMenu = () => {
+    document.getElementById("sideMenu").style.width = "0%";
+  };
+
+  const navLinks = [
+    { title: "Transfer", route: "/" },
+    { title: "Project Documentation", route: "/project-documentation" },
+  ];
   return (
-    <section className={classes.container}>
-      <div className={classes.logoSection}>Integrations Assessment</div>
-      <div className={classes.navLinkSection}>
-        <Link to="/">Transfer</Link>
-        <Link to="/transaction-history">Transactions history</Link>
+    <section className={classes.outerContainer}>
+      <div className={classes.container}>
+        <div className={classes.logoSection}>Integrations Assessment</div>
+        <div className={classes.navLinkSection}>
+          {navLinks.map((link) => {
+            return (
+              <Link
+                to={link.route}
+                key={link.title}
+                className={
+                  window.location.pathname === link.route && classes.activeLink
+                }
+              >
+                {link.title}
+              </Link>
+            );
+          })}
+        </div>
+        <div className={classes.hamburgerMenu}>
+          <FontAwesomeIcon icon={faBars} onClick={openSideMenu} />
+        </div>
+      </div>
+
+      {/* Sidenav */}
+      <div className={classes.sideNav} id="sideMenu">
+        <div className={classes.sideNavInner}>
+          <div>
+            <FontAwesomeIcon icon={faXmark} onClick={closeSideMenu} />
+          </div>
+          {navLinks.map((link) => {
+            return (
+              <Link
+                to={link.route}
+                key={link.title}
+                className={
+                  window.location.pathname === link.route && classes.activeLink
+                }
+              >
+                {link.title}
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
